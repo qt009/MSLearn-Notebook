0694d3960c72b5c7b65c374fc58f11910832b5a2
Quang Tran <quangtd.09092001@gmail.com>
2026-06-30 11:58:48 +0200

    fix app start error

--- backend/app/scraper/routes.py
+++ backend/app/scraper/routes.py
@@ -1,10 +1,12 @@
 from app.services.HTTPClient import HTTPClient
 from app.scraper.helper import getAccessToken
 from fastapi import APIRouter
-router = APIRouter()
+router = APIRouter(
+    prefix="/api/scraper",
+    tags=["Scraper"]
+)
 
-
-@router.post("/scrap/{cert_id}")
+@router.post("/scrape/{cert_id}")
 def scrap_certificate(cert_id):
     token = getAccessToken()
     print(token)
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from backend!")
+from fastapi import FastAPI
+from app.scraper.routes import router as ScraperRouter
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+app.include_router(ScraperRouter)
 
 
-if __name__ == "__main__":
-    main()
